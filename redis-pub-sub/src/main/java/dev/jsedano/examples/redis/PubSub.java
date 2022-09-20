c381822ab482007fd0bd21674733e17a9d3de861
jsedano <jsedano.dev@gmail.com>
2022-09-20 00:21:30 -0500

    ran fmt

--- redis-pub-sub/src/main/java/dev/jsedano/examples/redis/PubSub.java
+++ redis-pub-sub/src/main/java/dev/jsedano/examples/redis/PubSub.java
@@ -1,13 +1,12 @@
 package dev.jsedano.examples.redis;
 
 import dev.jsedano.examples.redis.listener.LogPubSub;
-import redis.clients.jedis.JedisPooled;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import redis.clients.jedis.JedisPooled;
 
 public class PubSub {
 
@@ -15,30 +14,23 @@ public class PubSub {
     JedisPooled jedis = new JedisPooled("localhost", 6379);
     ExecutorService executor = Executors.newFixedThreadPool(4);
 
-
     executor.execute(() -> jedis.subscribe(new LogPubSub("onlyOne"), "dev.one"));
     executor.execute(() -> jedis.subscribe(new LogPubSub("oneAndTwo"), "dev.one", "dev.two"));
     executor.execute(() -> jedis.psubscribe(new LogPubSub("pattern"), "dev.*"));
 
-
-      String message = "";
-      try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-        do {
-          message = br.readLine();
-          int firstSpace = message.indexOf(' ');
-          if(firstSpace > 1){
-            jedis.publish(message.substring(0, firstSpace), message.substring(firstSpace+1));
-          }
-        } while (!"close".equals(message));
-        jedis.close();
-        System.exit(0);
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
+    String message = "";
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      do {
+        message = br.readLine();
+        int firstSpace = message.indexOf(' ');
+        if (firstSpace > 1) {
+          jedis.publish(message.substring(0, firstSpace), message.substring(firstSpace + 1));
+        }
+      } while (!"close".equals(message));
+      jedis.close();
+      System.exit(0);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
-
-
-
-
-  }
-
+}
