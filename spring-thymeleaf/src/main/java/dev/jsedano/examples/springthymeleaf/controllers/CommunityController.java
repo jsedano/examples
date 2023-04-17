edfc2cf943a545c39d81db73b0cd80a1a0205142
jsedano <jsedano.dev@gmail.com>
2023-04-16 22:53:35 -0600

    spring thymeleaf

--- spring-thymeleaf/src/main/java/dev/jsedano/examples/springthymeleaf/controllers/CommunityController.java
+++ spring-thymeleaf/src/main/java/dev/jsedano/examples/springthymeleaf/controllers/CommunityController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,21 +21,42 @@ public class CommunityController {
   private final CommunityRepository communityRepository;
 
   @RequestMapping("/home")
-  public String displayProjects(Model model) {
+  public String displayCommunities(Model model) {
     Iterable<Community> communityIterable = communityRepository.findAll();
-    model.addAttribute("communityList", communityIterable);
+    model.addAttribute("communityList", communityRepository.findAll());
     model.addAttribute("community", new Community());
     return "community";
   }
 
   @PostMapping("/save")
-  public String createProject(@Valid Community community, Errors errors, Model model) {
+  public String createCommunity(@Valid Community community, Errors errors, Model model) {
     if (errors.hasErrors()) {
       model.addAttribute("communityList", communityRepository.findAll());
       return "community";
     }
-    log.info("saving {}", community);
     communityRepository.save(community);
     return "redirect:/community/home";
+  }
+
+  @PostMapping("/update")
+  public String updateCommunity(@Valid Community community, Errors errors, Model model) {
+    if (errors.hasErrors()) {
+      return "update";
+    }
+    log.info("to update: {}", community.toString());
+    communityRepository.save(community);
+    return "redirect:/community/home";
+  }
+
+  @RequestMapping("/delete/{id}")
+  public String deleteCommunity(@PathVariable Long id) {
+    communityRepository.deleteById(id);
+    return "redirect:/community/home";
+  }
+
+  @RequestMapping("/update/{id}")
+  public String updateProject(@PathVariable Long id, Model model) {
+    model.addAttribute("community", communityRepository.findById(id));
+    return "update";
   }
 }
